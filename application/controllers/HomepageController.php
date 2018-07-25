a4a72c97b08475ad74a36c6ae66564b026754667
Hammour ihab <hammourihab@yandex.com>
2018-07-25 17:13:18 +0100

    added HomepageController

---
+++ application/controllers/HomepageController.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class HomepageController extends CI_Controller {
+
+	public function index()
+	{
+		/**
+		 * By default, the boilerplate is using Codeigniter default view loaders
+		 * Thus i have made a view helper to avoid loading important layouts such as the navigation and the footer
+		 * Feel free to use it lol. 
+		 */
+		$this->load->view('homepage')
+	}
+}
